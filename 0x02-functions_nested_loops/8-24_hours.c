9724b0028b342fe323dd2a0221bc76592516d461
OPunga <ronnyopunga96@gmail.com>
2022-03-15 16:14:58 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,24 +8,27 @@
 void jack_bauer(void)
 {
 
-	int h1;
-	int h2;
-	int m1;
-	int m2;
-	int a = 9;
+int h1;
+int h2;
+int m1;
+int m2;
+int a = 9;
 
-	h2 = 0;
-	while (h2 <= 2)
+h2 = 0;
+while (h2 <= 2)
+{
+	if (h2 == 2)
 	{
-		if (h2 == 2)
+		/*Restrain to 23h, not 29*/
+		a = 3;
+	}
+	h1 = 0;
+	while (h1 <= a)
+	{
+		m2 = 0;
+		while (m2 <= 5)
 		{
-			/*Restrain to 23h, not 29*/
-			a = 3;
-		}
-		h1 = 0;
-		while (h1 <= a)
-		{
-			m2 = 0;
+			m1 = 0;
 			while (m1 <= 9)
 			{
 				_putchar('0' + h2);
@@ -41,4 +44,6 @@ void jack_bauer(void)
 		h1++;
 	}
 	h2++;
+}
+
 }
